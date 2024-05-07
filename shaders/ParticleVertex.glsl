a7548d848b65062b9f3c3f7407a876672bf06e7f
JakeHo0310 <160144686+JakeHo0310@users.noreply.github.com>
2024-05-07 19:09:49 +0100

    Update ParticleVertex.glsl

--- shaders/ParticleVertex.glsl
+++ shaders/ParticleVertex.glsl
@@ -2,6 +2,7 @@
 
 layout (location=0) in vec3 inPos;
 layout (location=1) in vec3 inColour;
+layout (location=2) in float inSize;
 out vec3 particleColour;
 
 uniform mat4 MVP;
@@ -9,4 +10,5 @@ void main()
 {
     gl_Position=MVP*vec4(inPos,1);
     particleColour=inColour;
+    gl_PointSize=inSize;
 }
