7535685c5e196e0c8a9bda65b35ff22d51cf6923
JakeHo0310 <160144686+JakeHo0310@users.noreply.github.com>
2024-05-11 20:09:04 +0100

    Add files via upload

---
+++ src/Emitter.cpp
@@ -0,0 +1,254 @@
+#include "Emitter.h"
+#include <ngl/Random.h>
+#include <fmt/format.h>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <ngl/Util.h>
+#include <ngl/Transformation.h>
+#include <ngl/ShaderLib.h>
+#include <ngl/VAOPrimitives.h>
+#include <ngl/VAOFactory.h>
+
+Emitter::Emitter(int _numParticles, int _maxAlive, int _colourType)
+{
+    m_numParticles=_numParticles;
+    m_maxAlive=_maxAlive;
+    m_colourType=_colourType;
+    pos.resize(m_numParticles);
+    dir.resize(m_numParticles);
+    colour.resize(m_numParticles);
+    life.resize(m_numParticles);
+    //size.resize(m_numParticles);
+    isAlive.resize(m_numParticles);
+    for(size_t i=0; i<m_numParticles; ++i)
+    {
+        createZeroParticle(i);
+    }
+    m_vao = ngl::vaoFactoryCast<ngl::MultiBufferVAO>(ngl::VAOFactory::createVAO(ngl::multiBufferVAO,GL_POINTS));
+    m_vao->bind();
+    m_vao->setData(ngl::MultiBufferVAO::VertexData(0,0)); //positions
+    m_vao->setData(ngl::MultiBufferVAO::VertexData(1,0)); //colours
+    m_vao->unbind();
+}
+
+void Emitter::createZeroParticle(size_t _p)
+{
+    isAlive[_p] = false;
+}
+
+void Emitter::createDefaultParticle(size_t _p)
+{
+  pos[_p]=m_position + randomVectorOnRectangle();
+  //dir[_p]=m_emitDir * ngl::Random::randomPositiveNumber() +randomVectorOnSphere() * m_spread;
+  dir[_p].m_y = -2.0;
+    life[_p] = static_cast<int>(500.0f+ngl::Random::randomPositiveNumber(750));
+  pos[_p].m_y= 40.0f;
+  pos[_p].m_w= 5.0f+ngl::Random::randomPositiveNumber(15);
+  isAlive[_p] = true;
+}
+
+
+ngl::Vec3 Emitter::randomVectorOnSphere()
+{
+  auto phi = ngl::Random::randomPositiveNumber(M_PI * 2.0f);
+  auto costheta = ngl::Random::randomNumber();
+  auto theta = acosf(costheta);
+  auto u = ngl::Random::randomPositiveNumber();
+  auto r = 1.0f *std::cbrt(u);
+  return ngl::Vec3(r*sinf(theta) *cosf(phi),
+                   r* sinf(theta) * sinf(phi),
+                   r*cosf(theta));
+}
+
+FlowField::FlowField(int _width, int _height, int _depth)
+        : m_width(_width), m_height(_height), m_depth(_depth)
+{
+    // Initialize Perlin modules with appropriate parameters
+    m_perlinModuleX.SetOctaveCount(8); // Increase octave count for X component
+    m_perlinModuleX.SetPersistence(0.6); // Adjust persistence for X component
+    m_perlinModuleY.SetOctaveCount(8); // Increase octave count for Y component
+    m_perlinModuleY.SetPersistence(0.6); // Adjust persistence for Y component
+    m_perlinModuleZ.SetOctaveCount(8); // Increase octave count for Z component
+    m_perlinModuleZ.SetPersistence(0.6); // Adjust persistence for Z component
+}
+
+ngl::Vec3 FlowField::getFlowDirection(float _x, float _y, float _z)
+{
+
+
+    // Use libnoise Perlin modules to generate flow direction
+    float flowX = m_perlinModuleX.GetValue(_x, _y, _z);
+    float flowY = m_perlinModuleY.GetValue(_x, _y, _z);
+    float flowZ = m_perlinModuleZ.GetValue(_x, _y, _z);
+
+    // Scale the input coordinates to adjust the frequency
+
+ //  simple smoothing by averaging neighboring samples
+    constexpr int numSamples = 1.2;
+    float smoothX = 0.0f;
+    float smoothY = 0.0f;
+    float smoothZ = 0.0f;
+
+    for (int dx = -numSamples; dx <= numSamples; ++dx)
+    {
+        for (int dy = -numSamples; dy <= numSamples; ++dy)
+        {
+            for (int dz = -numSamples; dz <= numSamples; ++dz)
+            {
+                smoothX += m_perlinModuleX.GetValue(_x + dx, _y + dy, _z + dz);
+                smoothY += m_perlinModuleY.GetValue(_x + dx, _y + dy, _z + dz);
+                smoothZ += m_perlinModuleZ.GetValue(_x + dx, _y + dy, _z + dz);
+            }
+        }
+    }
+
+    // Normalize the smoothed values
+    flowX = smoothX / pow(2 * numSamples + 1, 3);
+    flowY = smoothY / pow(2 * numSamples + 1, 3);
+    flowZ = smoothZ / pow(2 * numSamples + 1, 3);
+
+    // Normalize flow direction vector
+    ngl::Vec3 flowDirection(flowX, flowY, flowZ);
+    flowDirection.normalize();
+    return flowDirection;
+}
+
+void Emitter::render() const
+{
+  glEnable(GL_PROGRAM_POINT_SIZE);
+  m_vao->bind();
+  m_vao->setData(ngl::AbstractVAO::VertexData(m_numParticles*sizeof(ngl::Vec4),pos[0].m_x));
+  m_vao->setVertexAttributePointer(0,4,GL_FLOAT,0,0);
+
+  m_vao->setData(ngl::AbstractVAO::VertexData(m_numParticles*sizeof(ngl::Vec3),colour[0].m_x));
+  m_vao->setVertexAttributePointer(1,3,GL_FLOAT,0,0);
+
+  m_vao->setNumIndices(m_numParticles);
+  m_vao->draw();
+  m_vao->unbind();
+// Calculate background gradient color based on particle positions
+    // Interpolate between top and bottom gradient colors based on particle's y-coordinate
+//    float gradientFactor;
+//    for(size_t p = 0; p < m_numParticles; ++p)
+//    {
+//        if(isAlive[p])
+//        {
+//            // Calculate gradient factor based on particle's y-coordinate
+//            gradientFactor = (pos[p].m_y + 50.0f) / 100.0f; // Assuming the scene height is 100 units
+//
+//            // Interpolate between top and bottom gradient colors
+//            ngl::Vec4 backgroundGradientColor = m_gradientTopColor * (1.0f - gradientFactor) + m_gradientBottomColor * gradientFactor;
+//
+//            // Render the background color at the particle's position
+//            renderBackgroundAtPosition(backgroundGradientColor, pos[p]);
+//
+//            // Now render particles or any other scene geometry on top of the background
+//            // ...
+//        }
+//    }
+}
+
+void Emitter::update()
+{
+  //std::cout<<"update\n";
+//// dir += gravity * _dt * 0.5f
+////  pos += p.dir * _dt
+float _dt= 0.3f;
+FlowField flowField(75.0f,40.0f,75.0f);
+ngl::Vec3 gravity(0,-0.7, 0);
+//static int numP =0;
+//// choose number to birth
+// find first not alive and set as new particle
+int numberToBirth=1000+ngl::Random::randomPositiveNumber(1500);
+
+for(int i=0; i<numberToBirth; ++i)
+{
+    size_t index=0;
+    for(auto a: isAlive)
+    {
+        if (!a)
+            break;
+        else
+            ++index;
+    }
+    if(index<m_numParticles)
+    {
+        createDefaultParticle(index);
+    }
+
+}
+
+    for(size_t p=0;p<m_numParticles; ++p)
+    {
+        if(isAlive[p])
+        {
+            // Generate a random value between -0.05 and 0.05 for horizontal change
+            //float horizontalVelocity = -1.0f + 8.0f * ngl::Random::randomPositiveNumber();
+            ngl::Vec3 currentPosition = ngl::Vec3(pos[p].m_x, pos[p].m_y, pos[p].m_z);
+
+            // Get flow direction at the current position from the flow field
+            ngl::Vec3 flowDirection = flowField.getFlowDirection(currentPosition.m_x, currentPosition.m_y, currentPosition.m_z);
+
+            // Update particle direction based on flow direction
+            dir[p] = flowDirection * _dt + gravity;
+            //dir[p].m_x = horizontalVelocity;
+            //dir[p] += gravity * _dt;
+            pos[p] += dir[p] * _dt ;
+            //pos[p].m_w += 0.05; //change size of particle
+            if(m_colourType == 0)
+            {
+                colour[p] = ngl::Vec3(0.8f, 0.8f, 1.0f);
+                glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            }
+            if(m_colourType == 1)
+            {
+                colour[p] = ngl::Vec3(1.0f,1.0f,1.0f);
+                glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            }
+            if(m_colourType == 2)
+            {
+                colour[p] = ngl::Vec3(1.0f,0.0f,0.0f);
+                glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            }
+            if(m_colourType == 3)
+            {
+                colour[p] = ngl::Vec3(0.3f+0.7f*ngl::Random::randomPositiveNumber(),
+                                      0.3f+0.7f*ngl::Random::randomPositiveNumber(),
+                                      0.3f+0.7f*ngl::Random::randomPositiveNumber());
+                glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            }
+            if(m_colourType == 4)
+            {
+                colour[p] = ngl::Vec3(0.0f,0.0f,0.0f);
+                glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            }
+            if (--life[p] == 0 )
+            {
+                createZeroParticle(p);
+            }
+        }
+    }
+}
+
+
+  //for(auto &p : m_particles)
+  //{
+    //if (p.isAlive == true)
+    //{
+      //p.dir += gravity * _dt * 0.5;
+      //p.pos += p.dir * _dt;
+      //p.size += 0.01f;
+
+      //if (--p.life == 0 || p.pos.m_y <= 0.0)
+      //{
+         //createZeroParticle(p);
+      //}
+    //}
+  //}
+//}
+
+
+
+
+
---
+++ src/NGLScene.cpp
@@ -0,0 +1,194 @@
+#include <QMouseEvent>
+#include <QGuiApplication>
+
+#include "NGLScene.h"
+#include <ngl/NGLInit.h>
+#include <ngl/VAOPrimitives.h>
+#include <ngl/Util.h>
+#include <ngl/ShaderLib.h>
+#include <iostream>
+#include <ngl/Texture.h>
+
+
+NGLScene::NGLScene()
+{
+  // re-size the widget to that of the parent (in this case the GLFrame passed in on construction)
+  setTitle("Snow Particles");
+}
+
+
+NGLScene::~NGLScene()
+{
+  std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
+}
+
+
+
+void NGLScene::resizeGL(int _w , int _h)
+{
+  m_win.width  = static_cast<int>( _w * devicePixelRatio() );
+  m_win.height = static_cast<int>( _h * devicePixelRatio() );
+  m_project = ngl::perspective(45.0f,float(_w)/_h,0.1f,200);
+}
+
+
+void NGLScene::initializeGL()
+{
+  // we must call that first before any other GL commands to load and link the
+  // gl commands from the lib, if that is not done program will crash
+  ngl::NGLInit::initialize();
+
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);			   // Black Background
+  // enable depth testing for drawing
+  glEnable(GL_DEPTH_TEST);
+  // enable multisampling for smoother drawing
+  glEnable(GL_MULTISAMPLE);
+
+  m_emitter=std::make_unique<Emitter>(500,400,0);
+  ngl::ShaderLib::loadShader("ParticleShader","shaders/ParticleVertex.glsl","shaders/ParticleFragment.glsl");
+  ngl::ShaderLib::use("ParticleShader");
+  // Import snow texture
+  ngl::Texture t("texture/p2.png");
+  m_ptexture = t.setTextureGL();
+
+    // passing in camera parameters from the camera class
+    ngl::Vec3 camera_position(5.0f, 0.0f, 5.0f);
+    ngl::Vec3 camera_direction(0.0f, 0.0f, 0.0f);
+    ngl::Vec3 camera_orientation(0.0f, 1.0f, 0.0f);
+    Camera camera(camera_position, camera_direction, camera_orientation);
+    m_view = camera.getViewMatrix();
+
+    //float cone_height = 20.0f; // Adjust height as needed
+    //float cone_radius = 10.0f; // Adjust radius as needed
+    //DoubleCone doubleCone(cone_height, cone_radius);
+       // Generate vertices for the double cone
+       //std::vector<ngl::Vec3> coneVertices = doubleCone.generateVertices(camera);
+
+  //m_view = ngl::lookAt({0,100,100},{0,0,0},{0,1,0});
+  startTimer(10);
+}
+
+void NGLScene::timerEvent(QTimerEvent *_event)
+{
+  m_emitter->update();
+  update();
+}
+
+void NGLScene::paintGL()
+{
+  // clear the screen and depth buffer
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glViewport(0,0,m_win.width,m_win.height);
+
+    auto rotX = ngl::Mat4::rotateX(m_win.spinXFace);
+  auto rotY = ngl::Mat4::rotateY(m_win.spinYFace);
+  auto mouseRotation = rotX * rotY;
+  mouseRotation.m_m[3][0] = m_modelPos.m_x;
+  mouseRotation.m_m[3][0] = m_modelPos.m_x;
+  mouseRotation.m_m[3][0] = m_modelPos.m_x;
+
+  ngl::ShaderLib::setUniform("MVP",m_project*m_view*mouseRotation);
+  glPointSize(2);
+  glBindTexture(GL_TEXTURE_2D, m_ptexture);
+  m_emitter->render();
+  //ngl::VAOPrimitives::draw("sphere");
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+//void renderBackgroundAtPosition(const ngl::Vec4& color, const ngl::Vec4& position) const
+//{
+//    // Enable depth testing
+//    glEnable(GL_DEPTH_TEST);
+//    glDepthFunc(GL_LEQUAL);
+//
+//    // Disable depth writing
+//    glDepthMask(GL_FALSE);
+//
+//    // Set up the shader program for rendering
+//    // Assuming you have a shader program named "backgroundShader"
+//    ngl::ShaderLib::use("backgroundShader");
+//
+//    // Set uniform for background color
+//    ngl::ShaderLib::setUniform("backgroundColor", color);
+//
+//    // Set uniform for position
+//    ngl::ShaderLib::setUniform("backgroundPosition", position);
+//
+//    // Render a fullscreen quad using modern OpenGL
+//
+//    // Define vertex positions for a fullscreen quad
+//    static const GLfloat quadVertices[] = {
+//            -1.0f, -1.0f, 0.0f, // Bottom-left corner
+//            1.0f, -1.0f, 0.0f, // Bottom-right corner
+//            1.0f,  1.0f, 0.0f, // Top-right corner
+//            -1.0f,  1.0f, 0.0f  // Top-left corner
+//    };
+//
+//    // Bind and enable VAO for rendering
+//    GLuint vao;
+//    glGenVertexArrays(1, &vao);
+//    glBindVertexArray(vao);
+//
+//    // Generate vertex buffer object (VBO) for vertex positions
+//    GLuint vbo;
+//    glGenBuffers(1, &vbo);
+//    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+//
+//    // Set up vertex attribute pointers
+//    glEnableVertexAttribArray(0);
+//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+//
+//    // Draw the quad
+//    glDrawArrays(GL_QUADS, 0, 4);
+//
+//    // Clean up
+//    glDeleteBuffers(1, &vbo);
+//    glDeleteVertexArrays(1, &vao);
+//
+//    // Clean up
+//    glDepthMask(GL_TRUE);
+//    glDisable(GL_DEPTH_TEST);
+//}
+
+void NGLScene::keyPressEvent(QKeyEvent *_event)
+{
+  // this method is called every time the main window recives a key event.
+  // we then switch on the key value and set the camera in the GLWindow
+  switch (_event->key())
+  {
+  // escape key to quite
+  case Qt::Key_Escape : QGuiApplication::exit(EXIT_SUCCESS); break;
+  case Qt::Key_Space :
+      m_win.spinXFace=0;
+      m_win.spinYFace=0;
+      m_modelPos.set(ngl::Vec3::zero());
+  break;
+
+  case Qt::Key_1 :
+      m_emitter=std::make_unique<Emitter>(500,400,0);
+  break;
+
+  case Qt::Key_2 :
+      m_emitter=std::make_unique<Emitter>(500,400,1);
+  break;
+
+  case Qt::Key_3 :
+      m_emitter=std::make_unique<Emitter>(500,400,2);
+  break;
+
+  case Qt::Key_4 :
+      m_emitter=std::make_unique<Emitter>(500,400,3);
+  break;
+
+  case Qt::Key_5 :
+      m_emitter=std::make_unique<Emitter>(500,400,4);
+  break;
+  
+  default : break;
+  }
+  // finally update the GLWindow and re-draw
+
+    update();
+}
